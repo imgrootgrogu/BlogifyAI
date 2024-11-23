7972a09f5e5a081f274984157c47c17d569ae02f
LeeLeeLee <lililiq888@gmail.com>
2024-11-22 22:01:23 -0500

    update custom model, lora and vae

--- application.py
+++ application.py
@@ -260,6 +260,5 @@ def logout():
     flash('You have been logged out.', 'info')
     return redirect(url_for('login'))
     
-if __name__ == '_main_':
-    port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    application.run(host="0.0.0.0", port=5000, debug=True)
